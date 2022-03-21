a3b62bca5df9363989b8f7b815cba54d2b215161
Gopi-Subedi <gopi.subedi09@gmail.com>
2022-03-21 18:02:33 +0545

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const decrementHandler = (e) => {
+    e.preventDefault();
+    count ? setCount(count - 1) : console.log('Count is empty!');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="app-content">
+        <div className="header">React Counting Interface</div>
+        <div className="body">
+          <button className="btn-substract" onClick={decrementHandler}>
+            -
+          </button>
+          <p className="result">{count}</p>
+          <button className="btn-add" onClick={(e) => setCount(count + 1)}>
+            +
+          </button>
+        </div>
+        <button className="btn-reset" onClick={(e) => setCount(0)}>
+          reset
+        </button>
+        <div className="count-rlt">
+          <p>Count</p>
+          <p className="result">{count}</p>
+        </div>
+      </div>
     </div>
   );
 }
